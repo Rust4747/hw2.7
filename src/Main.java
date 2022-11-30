ba2bd81f150e4ef34d035cc86ddd3bc7a8f667c8
Tony <yaraliev5@gmail.com>
2022-11-30 13:13:03 +0300

    hw2.7

--- src/Main.java
+++ src/Main.java
@@ -34,7 +34,7 @@ public class Main {
 
     private static void add(String name, Queue<String> queue1,Queue<String> queue2) {
         if (queue1.size()==MAX_SIZE && queue2.size()==MAX_SIZE){
-            System.out.println("Гаааляяя");
+            System.out.println("Гаааляя");
 
         }
         if (queue1.size()<queue2.size()){
